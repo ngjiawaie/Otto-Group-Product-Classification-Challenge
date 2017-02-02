dea73decb45cc5bf72df31d6852625f79a93e9bb
Teo Zheng <Teo Zheng>
2017-02-02 22:28:06 +0800

    implement random forest

--- tree.R
+++ tree.R
@@ -2,7 +2,9 @@
 rm(list=ls())
 library(ISLR)
 library(tree)
-library(rpart)
+library(randomForest)
+library(ggplot2)
+library(dplyr)
 set.seed(5)
 
 # import data
@@ -14,66 +16,59 @@ dim(train_data)
 dim(test_data)
 head(train_data)
 head(test_data)
+str(train_data)
 #test_data no "target", hence train and test data need to be created from train_data
 
 #delete id column first, don't want t to be included
 train_data <- train_data[,-1]
 
 #create train and test data from train_data
-train <- sample(1:nrow(train_data),nrow(train_data)/2)
-test <- -train
-train_d <- train_data[train,]
+test <- sample(1:nrow(train_data),nrow(train_data)/2)
 test_d <- train_data[test,]
 test_target <- test_d$target
+#downsampling is use to make sure balanced "target" class is obtained
+train_d <- downSample(x = train_data[, -ncol(train_data)],y = train_data$target)
+table(train_d$Class)
 
 #try with tree
-tree_model <- tree(target ~., train_d) 
-summary(tree_model)
-plot(tree_model)
-text(tree_model, pretty = 0)
-#printcp(tree_model)
+x <- train_d
+x$Class <- NULL
+tunemtry <- tuneRF(x,train_d$Class, stepFactor=1.5, plot=TRUE,improve=0.01)
+#tunemtry shows the best mtry is 19
+tree_model <- randomForest(Class ~ ., train_d, importance=TRUE, mtry=19) 
+print(tree_model)
+plot(tree_model, main="Tree")
 
 #test the model by using predict
 tree_predict <- predict(tree_model, test_d, type="class")
-mean(tree_predict != test_target) #0.4144607, abit high
+mean(tree_predict != test_target) #around 0.17
 
-#prune, start with cross validation
-set.seed(3)
-cv_tree <- cv.tree(tree_model, FUN = prune.misclass)
-names(cv_tree)
-plot(cv_tree$size , cv_tree$dev, type="b")
-#best is 9
+#view confusion Matrix
+confusionMatrix(tree_predict,test_target)
 
-#prune
-prune_model <- prune.misclass(tree_model, best = 9)
-plot(prune_model)
-text(prune_model, pretty=0)
+#importance
+imp <- importance(tree_model, type=1)
+ShowImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+#ggplot, arranged according to the importance value
+p <- ggplot(ShowImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+  geom_bar(stat="identity", fill="#32CD32") + #identity represent values
+  coord_flip() + 
+  xlab("Features") +
+  ylab("Importance") + 
+  ggtitle("Importance of Features \n") +
+  theme(plot.title=element_text(size=20))
+p #convert to pdf and increase the length to get a clearer view
 
-#check prune_model
-tree_predict <- predict(prune_model, test_d, type="class")
-mean(tree_predict != test_target)
-#nothing is pruned, cause initial tree start with 9(lowest error rate) alrdy
+#too many, we only interested in top 10 features
+imp.ordered <- arrange(ShowImportance, -Importance)
+imp.top10 <- imp.ordered[1:10,]
+p_mini <- ggplot(imp.top10, aes(x=reorder(Feature, Importance), y=Importance)) +
+  geom_bar(stat="identity", fill="#32CD32") + #identity represent values
+  coord_flip() + 
+  xlab("Features") +
+  ylab("Importance") + 
+  ggtitle("Importance of Features \n") +
+  theme(plot.title=element_text(size=20))
+p_mini
 
-#try with rpart to see if it delivers a better value
-set.seed(4)
-tree_model2 <- rpart(target ~., data = train_d, method="class") 
-summary(tree_model2)
-plot(tree_model2)
-text(tree_model2, pretty = 0)
-printcp(tree_model2)
-plotcp(tree_model2, upper="size")
 
-tree_predict2 <- predict(tree_model2, test_d, type="class")
-mean(tree_predict2 != test_target)
-#0.387763, lower compare to tree
-
-#prune tree according to the cp of lowest xerror(lowest error rate)
-prune_tree2<- prune(tree_model2, cp = tree_model2$cptable[which.min(tree_model2$cptable[,"xerror"]),"CP"])
-printcp(prune_tree2)
-plot(prune_tree2,uniform=TRUE)
-text(prune_tree2,pretty = 0)
-
-#check how the predict perform
-tree_predict2 <- predict(prune_tree2, test_d, type="class")
-mean(tree_predict2 != test_target)
-#0.387763
